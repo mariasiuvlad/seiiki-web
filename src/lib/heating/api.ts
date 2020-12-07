464ae725ab91ca60ed8e3c3257639095519fd0b0
Mariasiu Vlad <mariasiuvlad@gmail.com>
2020-12-07 13:39:27 +0200

    Update env variable name

--- src/lib/heating/api.ts
+++ src/lib/heating/api.ts
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-const BASE_URL = process.env.HEATING_API_URL || '';
+const BASE_URL = process.env.REACT_APP_HEATING_API_URL || '';
 
 const config = {baseURL: BASE_URL};
 const httpClient = Axios.create(config);
