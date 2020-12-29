a2ef66e75e8c1a5407dc2b4d2536c2f7d6be7f40
Mariasiu Vlad <mariasiuvlad@gmail.com>
2020-12-29 15:37:37 +0200

    Add ErrorBoundary reset

--- src/components/atoms/ReadingChart/Suspense.tsx
+++ src/components/atoms/ReadingChart/Suspense.tsx
@@ -5,7 +5,7 @@ import ReadingChartFallback, {ReadingChartError} from './Fallback'
 
 export default function ReadingChart(props) {
   return (
-    <ErrorBoundary fallback={<ReadingChartError {...props} />}>
+    <ErrorBoundary key={props.interval} fallback={<ReadingChartError {...props} />}>
       <Suspense fallback={<ReadingChartFallback {...props} />}>
         <ReadingChartContainer {...props} />
       </Suspense>
