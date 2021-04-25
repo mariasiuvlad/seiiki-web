d0bf8e4371a66f36e47138ec17075bbf417d18a0
Mariasiu Vlad <mariasiuvlad@gmail.com>
2021-04-25 23:54:49 +0300

    Add 1hour heating

--- src/components/molecules/TwoHours/index.tsx
+++ src/components/molecules/TwoHours/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import cx from 'classnames'
 import useHeatingAgent from 'hooks/useHeatingAgent'
+import {Row} from 'components/atoms/Flex'
 
 const TwoHours = ({className}) => {
   const {nHours} = useHeatingAgent()
 
   return (
-    <>
+    <Row className="gap-2">
       <button onClick={() => nHours(1)} className={cx(className, 'btn primary flex-1')}>
         1 hour
       </button>
       <button onClick={() => nHours(2)} className={cx(className, 'btn primary flex-1')}>
         2 hours
       </button>
-    </>
+    </Row>
   )
 }
 
